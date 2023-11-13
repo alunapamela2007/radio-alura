69ffbead0f36017a74e13fe6f21aede0bea3f578
alunapamela2007 <145346289+alunapamela2007@users.noreply.github.com>
2023-11-13 08:38:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert "OLÁ GALERA."
+function  tocaSom (idE LementoAudio{
+} 
+const listaDeTeclas = document.querySelectorAll(". tecla") ;
+
+for(let contador = 0; contador < listaDeteclas . length; contador ++){
+      const tecla = listaDeteclas [ contador ];
+      const efeito = tecla.classList[1];
+    const idAudio = `#som_${esfeito}`;
+      tecla.onclik = function (){ 
+        tocaSom idAudio);
+        }
+}
